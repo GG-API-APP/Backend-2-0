d4b65695d07384d92b6e126535370c5ef989b32a
GGBOT <nowaskorka@wp.pl>
2023-10-07 15:06:18 +0200

    test

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGODB_CONNECT_URI;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
-// Routes
-app.use("/home", home);
-
-// connection
+// Connection
 const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+app.get("/test", async (req, res) => {
+  try {
+    const database = client.db("sample_airbnb");
+    const collection = database.collection("listingsAndReviews");
+
+    const result = await collection.findOne({});
+    res.json(result);
+  } catch (error) {
+    console.error("Error fetching data from MongoDB:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+  connectToMongoDB();
+});
